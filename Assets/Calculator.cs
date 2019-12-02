52f03f14bd831df9292f592d3d11766e19136631
Lewis Jones <lewisjones21@hotmail.com>
2019-12-02 20:00:44 +0000

    Initial commit

---
+++ Assets/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour
+{
+    private List<int> _inputs = new List<int>()
+    {
+        0
+    };
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log("Output is: " + Calculate().ToString());
+    }
+
+    // Update is called once per frame
+    private int Calculate()
+    {
+        return 0;
+    }
+}
