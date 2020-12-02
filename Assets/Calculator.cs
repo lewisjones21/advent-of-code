5afdcd06bda4ac9e6219c3c174f73ee83d7b6912
Lewis Jones <lewisjones21@hotmail.com>
2020-12-02 20:42:54 +0000

    Set up input reader and calculator process

--- Assets/Calculator.cs
+++ Assets/Calculator.cs
@@ -1,57 +1,14 @@
 ﻿using UnityEngine;
 
-public class Calculator : MonoBehaviour
+public class Calculator : CalculatorBase<int, int>
 {
-    private const int MIN = 278384, MAX = 824795;
-
-    private void Awake()
+    protected override int ParseInput(string inputLine)
     {
-        Debug.Log("Output is: " + Calculate().ToString());
+        return int.Parse(inputLine);
     }
 
-    private int Calculate()
+    protected override int CalculateOutput()
     {
-        int validCount = 0;
-        for (int i = MIN; i <= MAX; i++)
-        {
-            if (GetIsValid(i))
-            {
-                validCount++;
-            }
-        }
-        return validCount;
-    }
-
-    private bool GetIsValid(int value)
-    {
-        int maxDigitIndex = (int)Mathf.Log10(value) + 1, digitIndex = 0, digit = 1, lastDigit = 0,
-            removalSum = 0;
-
-        // Value must have 6 digits
-        if (maxDigitIndex != 6)
-        {
-            return false;
-        }
-
-        bool hasAdjacentMatch = false;
-        while (digitIndex < maxDigitIndex)
-        {
-            digit = (int)(value / Mathf.Pow(10, maxDigitIndex - (digitIndex + 1))) - removalSum;
-
-            // Digits may not decrease in size
-            if (digit < lastDigit)
-            {
-                return false;
-            }
-
-            // There must be at least one pair of matching digits
-            hasAdjacentMatch |= digit == lastDigit;
-
-            lastDigit = digit;
-            digitIndex++;
-            removalSum += lastDigit;
-            removalSum *= 10;
-        }
-        return hasAdjacentMatch;
+        return 0;
     }
 }
---
+++ Assets/CalculatorBase.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CalculatorBase<TInput, TOutput> : MonoBehaviour
+{
+    [SerializeField]
+    protected string _inputFilename = "Assets/Input.txt";
+
+    [SerializeField]
+    protected List<TInput> _input = new List<TInput>();
+
+    [SerializeField]
+    protected TOutput _output = default;
+
+    private void Awake()
+    {
+        Process();
+    }
+
+    private void Process()
+    {
+        _input = FetchInput();
+        if (_input == null || _input.Count == 0)
+        {
+            Debug.LogError("Cannot calculate output; input is empty");
+            return;
+        }
+        else
+        {
+            Debug.LogFormat("Fetched {0} input entries", _input.Count);
+        }
+        _output = CalculateOutput();
+        Debug.Log("Output is: " + _output);
+    }
+
+    private List<TInput> FetchInput()
+    {
+        List<TInput> values = new List<TInput>();
+        if (string.IsNullOrWhiteSpace(_inputFilename))
+        {
+            Debug.LogError("Cannot fetch input; input filename is empty");
+            return values;
+        }
+        StreamReader reader = new StreamReader(_inputFilename);
+        while (!reader.EndOfStream)
+        {
+            values.Add(ParseInput(reader.ReadLine()));
+        }
+        return values;
+    }
+
+    protected abstract TInput ParseInput(string inputLine);
+
+    protected abstract TOutput CalculateOutput();
+}
