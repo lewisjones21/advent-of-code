28be724d49318abc2fd8407d41677c3380a83ffb
Lewis Jones <lewisjones21@hotmail.com>
2020-12-02 21:37:52 +0000

    Solve 1-1

--- Assets/Calculator.cs
+++ Assets/Calculator.cs
@@ -9,6 +9,17 @@ public class Calculator : CalculatorBase<int, int>
 
     protected override int CalculateOutput()
     {
+        for (int i = 0; i < _input.Count; i++)
+        {
+            for (int j = i + 1; j < _input.Count; j++)
+            {
+                if (_input[i] + _input[j] == 2020)
+                {
+                    return _input[i] * _input[j];
+                }
+            }
+        }
+        Debug.LogWarning("Could not find a valid output");
         return 0;
     }
 }
