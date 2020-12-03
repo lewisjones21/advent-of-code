e12aa1cadb65596c5840013deb4c37d33356c57d
Lewis Jones <lewisjones21@hotmail.com>
2020-12-03 13:35:20 +0000

    Solve 3-2

--- Assets/Calculator.cs
+++ Assets/Calculator.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections.Generic;
 
-public class Calculator : CalculatorBase<List<char>, int>
+public class Calculator : CalculatorBase<List<char>, long>
 {
     protected override List<char> ParseInputLine(string inputLine)
     {
         return new List<char>(inputLine.ToCharArray());
     }
 
-    protected override int CalculateOutput()
+    protected override long CalculateOutput()
     {
-        int treeHits = 0;
-        for (int i = 0, xPos = 0; i < _input.Count; i++, xPos += 3)
+        long accumulator = 0;
+        int[] slopeXValues = { 1, 3, 5, 7, 1 };
+        int[] slopeYValues = { 1, 1, 1, 1, 2 };
+        for (int s = 0; s < slopeXValues.Length; s++)
         {
-            if (_input[i][xPos % _input[i].Count] == '#')
+            int treeHits = 0;
+            for (int yPos = 0, xPos = 0;
+                 yPos < _input.Count;
+                 yPos += slopeYValues[s], xPos += slopeXValues[s])
             {
-                treeHits++;
+                if (_input[yPos][xPos % _input[yPos].Count] == '#')
+                {
+                    treeHits++;
+                }
+            }
+            if (accumulator == 0)
+            {
+                accumulator = treeHits;
+            }
+            else
+            {
+                accumulator *= treeHits;
             }
         }
-        return treeHits;
+        return accumulator;
     }
 }
