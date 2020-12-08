9637461384a219a352b4e4e7054a583d6766dfa6
Lewis Jones <lewisjones21@hotmail.com>
2020-12-08 01:42:40 +0000

    Solve 7-2

--- Assets/Calculator.cs
+++ Assets/Calculator.cs
@@ -30,59 +30,45 @@ public class Calculator : CalculatorBase<Tuple<string, List<Tuple<int, string>>>
     {
         string target = "shiny gold";
         // Determine whether each type of bag contains the target bag
-        // 0 - undetermined, 1 - yes, 2 - no
-        List<int> containsTarget = new List<int>(new int[_input.Count]);
-        for (int i = 0; i < _input.Count; i++)
+        // -1 - undetermined, n - contains n bags
+        List<int> containmentCounts = new List<int>(new int[_input.Count]);
+        for (int c = 0; c < containmentCounts.Count; c++)
         {
-            containsTarget[i] = ContainsTarget(target, _input, containsTarget, i) ? 1 : 2;
+            containmentCounts[c] = -1;
         }
-        // Count the number of different bag types which could contain the target
-        // (Do not count the target itself)
-        int sum = 0;
-        for (int c = 0; c < containsTarget.Count; c++)
-        {
-            if (containsTarget[c] == 1 && _input[c].Item1 != target)
-            {
-                sum++;
-            }
-        }
-
-        return sum;
+        // Find how many bags the target bag type contains
+        int targetIndex = _input.FindIndex((Tuple<string, List<Tuple<int, string>>> obj)
+                                           => obj.Item1 == target);
+        return ContainmentCount(_input, containmentCounts, targetIndex);
     }
 
-    private bool ContainsTarget(string target,
-                                List<Tuple<string, List<Tuple<int, string>>>> inputs,
-                                List<int> inputContainsTarget,
-                                int currentIndex,
-                                int recursionCount = 0)
+    private int ContainmentCount(List<Tuple<string, List<Tuple<int, string>>>> schema,
+                                 List<int> containmentCounts,
+                                 int currentIndex,
+                                 int recursionCount = 0)
     {
+        if (containmentCounts[currentIndex] >= 0)
+        {
+            return containmentCounts[currentIndex];
+        }
         if (recursionCount > 100)
         {
             Debug.LogError("Recursion level has reached 100; aborting calculation; " +
                            "results may be invalid");
-            return false;
+            return 0;
         }
-        if (inputContainsTarget[currentIndex] == 1 || inputs[currentIndex].Item1 == target)
+        int numContainedInThis = 0;
+        for (int i = 0; i < schema[currentIndex].Item2.Count; i++)
         {
-            return true;
+            // Find the index of the contained bag in the schema
+            int newIndex = schema.FindIndex((Tuple<string, List<Tuple<int, string>>> obj)
+                                            => obj.Item1 == schema[currentIndex].Item2[i].Item2);
+            // Find how many bags the contained bag contains, and multiply it by the number of
+            // instances of that contained bag, before adding to the total for the current bag
+            numContainedInThis += schema[currentIndex].Item2[i].Item1 * (1 + ContainmentCount(
+                schema, containmentCounts, newIndex, recursionCount + 1));
         }
-        else if (inputContainsTarget[currentIndex] == 2)
-        {
-            return false;
-        }
-        for (int i = 0; i < inputs[currentIndex].Item2.Count; i++)
-        {
-            int newIndex = inputs.FindIndex((Tuple<string, List<Tuple<int, string>>> obj)
-                                            => obj.Item1 == inputs[currentIndex].Item2[i].Item2);
-            if (ContainsTarget(
-                target, inputs, inputContainsTarget, newIndex, recursionCount + 1))
-            {
-
-                inputContainsTarget[currentIndex] = 1;
-                return true;
-            }
-        }
-        inputContainsTarget[currentIndex] = 2;
-        return false;
+        containmentCounts[currentIndex] = numContainedInThis;
+        return numContainedInThis;
     }
 }
