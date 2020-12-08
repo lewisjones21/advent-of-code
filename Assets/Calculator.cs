f995fe0001ad19f9e3b534e722f3a84f4c3d8ba7
Lewis Jones <lewisjones21@hotmail.com>
2020-12-08 14:08:40 +0000

    Solve 8-2

--- Assets/Calculator.cs
+++ Assets/Calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Calculator : CalculatorBase<Tuple<Calculator.Operation, int>, long>
 {
@@ -21,28 +22,65 @@ public class Calculator : CalculatorBase<Tuple<Calculator.Operation, int>, long>
 
     protected override long CalculateOutput()
     {
-        // Process the input instructions
-        int instruction = 0;
-        int accumulator = 0;
-        List<int> visitedInstructions = new List<int>();
-        while (!visitedInstructions.Contains(instruction))
+        Operation operation;
+        for (int changeIndex = _input.Count - 1; changeIndex >= 0; changeIndex--)
         {
-            visitedInstructions.Add(instruction);
-            switch (_input[instruction].Item1)
+            // Skip iterations where the target operation to change is not under consideration
+            if (_input[changeIndex].Item1 == Operation.Accumulate)
             {
-                case Operation.NoOperation:
-                    instruction++;
+                continue;
+            }
+            // Process the input instructions
+            int instruction = 0;
+            int accumulator = 0;
+            List<int> visitedInstructions = new List<int>();
+            while (!visitedInstructions.Contains(instruction))
+            {
+                if (instruction == _input.Count)
+                {
+                    Debug.LogFormat(
+                        "Program terminated correctly when instruction {0} was changed",
+                        changeIndex);
+                    return accumulator;
+                }
+                else if (instruction > _input.Count)
+                {
+                    Debug.LogErrorFormat(
+                        "Program terminated incorrectly when instruction {0} was changed; " +
+                        "ended {1} instructions out of range",
+                        changeIndex, instruction - _input.Count);
                     break;
-                case Operation.Accumulate:
-                    accumulator += _input[instruction].Item2;
-                    instruction++;
-                    break;
-                case Operation.Jump:
-                    instruction += _input[instruction].Item2;
-                    break;
+                }
+                visitedInstructions.Add(instruction);
+                operation = _input[instruction].Item1;
+                if (instruction == changeIndex)
+                {
+                    if (operation == Operation.NoOperation)
+                    {
+                        operation = Operation.Jump;
+                    }
+                    else
+                    {
+                        operation = Operation.NoOperation;
+                    }
+                }
+                switch (operation)
+                {
+                    case Operation.NoOperation:
+                        instruction++;
+                        break;
+                    case Operation.Accumulate:
+                        accumulator += _input[instruction].Item2;
+                        instruction++;
+                        break;
+                    case Operation.Jump:
+                        instruction += _input[instruction].Item2;
+                        break;
+                }
             }
         }
-        // Return the accumulator value when an instruction is first revisited
-        return accumulator;
+
+        Debug.LogError("No solution was found");
+        return 0;
     }
 }
