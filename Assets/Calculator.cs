d13ded6c280d77f3fd1304cbefc4c23f5f137f4e
Lewis Jones <lewisjones21@hotmail.com>
2020-12-21 21:47:57 +0000

    Solve 9-2

--- Assets/Calculator.cs
+++ Assets/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Calculator : CalculatorBase<long, long>
 {
@@ -12,7 +13,8 @@ public class Calculator : CalculatorBase<long, long>
         int searchRange = 25;
         // Determine which number cannot be created by summing two numbers in the previous range
         bool hasSolution;
-        for (int lastIndex = searchRange; lastIndex < _input.Count; lastIndex++)
+        int lastIndex;
+        for (lastIndex = searchRange; lastIndex < _input.Count; lastIndex++)
         {
             hasSolution = false;
             for (int index1 = lastIndex - searchRange; index1 < lastIndex - 1; index1++)
@@ -32,7 +34,24 @@ public class Calculator : CalculatorBase<long, long>
             }
             if (!hasSolution)
             {
-                return _input[lastIndex];
+                break;
+            }
+        }
+        // Determine the range of contiguous numbers that sum to the number discovered above
+        for (int startIndex = 0; startIndex < lastIndex; startIndex++)
+        {
+            int rangeWidth = 0;
+            long sum = 0;
+            while (sum < _input[lastIndex])
+            {
+                sum += _input[startIndex + rangeWidth];
+                rangeWidth++;
+            }
+            if (sum == _input[lastIndex])
+            {
+                List<long> range = _input.GetRange(startIndex, rangeWidth);
+                range.Sort();
+                return range[0] + range[range.Count - 1];
             }
         }
 
