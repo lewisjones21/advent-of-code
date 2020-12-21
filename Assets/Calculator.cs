919deb12a6d22142d29b6d4d4ffc08b4c2ccdeef
Lewis Jones <lewisjones21@hotmail.com>
2020-12-21 21:22:05 +0000

    Solve 9-1

--- Assets/Calculator.cs
+++ Assets/Calculator.cs
@@ -1,82 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Calculator : CalculatorBase<Tuple<Calculator.Operation, int>, long>
+public class Calculator : CalculatorBase<long, long>
 {
-    public enum Operation
+    protected override long ParseInputLine(string inputLine)
     {
-        NoOperation = 0,
-        Accumulate,
-        Jump
-    }
-
-    protected override Tuple<Operation, int> ParseInputLine(string inputLine)
-    {
-        string[] operationAndArgument = inputLine.Split(' ');
-        return new Tuple<Operation, int>(operationAndArgument[0] == "acc" ? Operation.Accumulate
-                                         : (operationAndArgument[0] == "jmp" ? Operation.Jump
-                                            : Operation.NoOperation),
-                                         int.Parse(operationAndArgument[1]));
+        return long.Parse(inputLine);
     }
 
     protected override long CalculateOutput()
     {
-        Operation operation;
-        for (int changeIndex = _input.Count - 1; changeIndex >= 0; changeIndex--)
+        int searchRange = 25;
+        // Determine which number cannot be created by summing two numbers in the previous range
+        bool hasSolution;
+        for (int lastIndex = searchRange; lastIndex < _input.Count; lastIndex++)
         {
-            // Skip iterations where the target operation to change is not under consideration
-            if (_input[changeIndex].Item1 == Operation.Accumulate)
+            hasSolution = false;
+            for (int index1 = lastIndex - searchRange; index1 < lastIndex - 1; index1++)
             {
-                continue;
-            }
-            // Process the input instructions
-            int instruction = 0;
-            int accumulator = 0;
-            List<int> visitedInstructions = new List<int>();
-            while (!visitedInstructions.Contains(instruction))
-            {
-                if (instruction == _input.Count)
+                for (int index2 = index1 + 1; index2 < lastIndex; index2++)
                 {
-                    Debug.LogFormat(
-                        "Program terminated correctly when instruction {0} was changed",
-                        changeIndex);
-                    return accumulator;
+                    if (_input[index1] + _input[index2] == _input[lastIndex])
+                    {
+                        hasSolution = true;
+                        break;
+                    }
                 }
-                else if (instruction > _input.Count)
+                if (hasSolution)
                 {
-                    Debug.LogErrorFormat(
-                        "Program terminated incorrectly when instruction {0} was changed; " +
-                        "ended {1} instructions out of range",
-                        changeIndex, instruction - _input.Count);
                     break;
                 }
-                visitedInstructions.Add(instruction);
-                operation = _input[instruction].Item1;
-                if (instruction == changeIndex)
-                {
-                    if (operation == Operation.NoOperation)
-                    {
-                        operation = Operation.Jump;
-                    }
-                    else
-                    {
-                        operation = Operation.NoOperation;
-                    }
-                }
-                switch (operation)
-                {
-                    case Operation.NoOperation:
-                        instruction++;
-                        break;
-                    case Operation.Accumulate:
-                        accumulator += _input[instruction].Item2;
-                        instruction++;
-                        break;
-                    case Operation.Jump:
-                        instruction += _input[instruction].Item2;
-                        break;
-                }
+            }
+            if (!hasSolution)
+            {
+                return _input[lastIndex];
             }
         }
 
